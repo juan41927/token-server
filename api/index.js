e3797ec90d1b07b325737ee0f9f70c3032067c8e
juan41927 <juan.manuel@narancobrands.com>
2022-01-31 14:48:32 +0100

    add push notification server

--- api/index.js
+++ api/index.js
@@ -6,6 +6,10 @@ const express = require("express");
 const cors = require("cors");
 const tokenGenerator = require("../src/token_generator");
 
+const FCM = require('fcm-node');
+const serverKey = "AAAA6AnqQas:APA91bFcmhBbw4WyX-iA5iB0g3d9U7cztiXWBT8ThVhasFV6Il8khM5kcSuF9HzGqUZfrY28mgkA7uI-ZrG931bvEEdNcPoySkQYydLbwaRYLHxTszDC4z305DR-WBfBMDMixxa-XX6b";
+const fcm = new FCM(serverKey);
+
 // Create Express webapp
 const app = express();
 
@@ -32,6 +36,37 @@ app.get("/api", (request, res) => {
   }
   const token = tokenGenerator(identity, room);
   res.send(token);
+});
+
+/** Recibe Push notification from mobile app */
+app.post('/send-push', (req, res) => {
+  const message = {
+    registration_ids: [...req.body.userFcmToken] ,  // array required
+    notification: {
+      title:req.body.notificationTitle ,
+      body: req.body.notificationBody,
+      sound:  "default",
+      icon: "ic_launcher",
+      badge:  req.body.notificationBadge ? req.body.notificationBadge : "1",
+      click_action: 'FCM_PLUGIN_ACTIVITY',
+    },
+    priority: req.body.notificationPriority ? req.body.notificationPriority : 'high',
+    data: {
+      action:req.body.actionType, // Action Type
+      payload:req.body.payload // payload
+    }
+  }
+     
+  fcm.send(message, (err, response) => {
+    if (err) {
+      console.log("Something has gone wrong!", JSON.stringify(err));
+      res.send(err);
+    } else {
+      console.log("Successfully sent with response: ", response);
+      res.send(response)
+    }
+  });
+
 });
 
 // production error handler
